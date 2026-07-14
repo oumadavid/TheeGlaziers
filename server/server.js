b481c84b39407cb48a68f327cad057ce676ee64e
oumadavid <daviddage438@gmail.com>
2026-07-14 19:23:57 +0300

    refactor: update the frontend url

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:8080", // your Vercel URL in production
+  origin: 'https://highlandglaziers.app', // your Vercel URL in production
 }));
 app.use(express.json());
 
